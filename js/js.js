3bb63e432246b328cc53d0aa9e9ff05c11242b81
Taonga Jere <jeretaonga.tj@gmail.com>
2024-01-05 15:29:23 +0200

    added event listeners, getting reference and updated function playRound

--- js/js.js
+++ js/js.js
@@ -1,50 +1,66 @@
 
 const choices = ["Rock", "Paper", "scissors"]
 
-
 // get computer choice function
-function getComputerChoice (Choices){
+function getComputerSelection (Choices){
     // the Math.random turns the random index into a whole number
     const randomIndex = Math.floor(Math.random() * choices.length);
-    let computerChoice = choices[randomIndex].toLowerCase();
+    let computerSelection = choices[randomIndex].toLowerCase();
 
-    return computerChoice
+    return computerSelection
 }
 
-const computerChose = getComputerChoice(choices)
+const computerSelection = getComputerSelection (choices)
 
+//getting reference button
+const rockbtn = document.getElementById('rockbtn');
+const scissorsbtn = document.getElementById('scissorsbtn');
+const paperbtn = document.getElementById('paperbtn');
 
-// get user choice
-let userChoice = prompt("Rock, Paper or Scissors?:" );
-userChoice = userChoice.toLocaleLowerCase();
-console.log("you choose: " + userChoice)
-console.log("computer chose: " + computerChose)
-
-// declair winner
-function game (userChoice, computerChose){
-    if (userChoice === computerChose){
-        return "Draw"
-    } else if (userChoice === 'rock'){
-        if (computerChose === 'scissors') {
+// function that will run after player selection
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Draw";
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
             return "Win, Rock beats scissors";
-        }else if (computerChose === paper) {
+        } else if (computerSelection === 'paper') {
             return "Lose, Paper covers Rock";
         }
-    } else if (userChoice === 'scissors') {
-        if (computerChose === 'paper') {
-            return "win, scissors cuts paper";
-        } else if (computerChose == 'rock') {
-            return "lose, Rock smashes scissors";
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            return "Win, Scissors cuts paper";
+        } else if (computerSelection === 'rock') {
+            return "Lose, Rock smashes scissors";
         }
-    } else if (userChoice === 'paper') {
-        if (computerChose === 'rock') {
-            return "win, paper covers rock";
-        } else if (computerChose === 'scissors') {
-            return "Lose, scissors cuts paper";
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            return "Win, Paper covers rock";
+        } else if (computerSelection === 'scissors') {
+            return "Lose, Scissors cuts paper";
         }
-    } else if (userChoice != 'rock', 'paper', 'scissors') {
-        return "enter either rock, paper or scissors"
+    } else {
+        return "Enter either rock, paper, or scissors";
     }
 }
 
-console.log(game (userChoice, computerChose));
+
+// event listener for each button
+let playerSelection; // declair playerSelection
+
+rockbtn.addEventListener ('click', function (){
+    playerSelection = 'rock';
+    const result = playRound ('rock', computerSelection)
+    console.log(result);
+});
+paperbtn.addEventListener ('click', function () {
+    playerSelection = 'paper'
+    const result = playRound ('paper', computerSelection)
+    console.log(result);
+});
+scissorsbtn.addEventListener ('click', function () {
+    playerSelection = 'scissors'
+    const result = playRound ('scissors', computerSelection)
+    console.log(result);
+});
+
